aa38ffc6bcc54752c2c10d37d92d6f16c066e505
MohamedMagdyOmar <eng_mohamed_magdy@hotmail.com>
2017-12-22 14:36:04 +0200

    refactor queries

---
+++ PostProcessingQueries.sql
@@ -0,0 +1,82 @@
+SET SQL_SAFE_UPDATES = 0;
+UPDATE diaconehotencoding SET DiacritizedCharacterOneHotEncoding = REPLACE(DiacritizedCharacterOneHotEncoding, ' ', '');
+UPDATE diaconehotencoding SET DiacritizedCharacterOneHotEncoding = REPLACE(DiacritizedCharacterOneHotEncoding, '[', '');
+UPDATE diaconehotencoding SET DiacritizedCharacterOneHotEncoding = REPLACE(DiacritizedCharacterOneHotEncoding, ']', '');
+UPDATE diaconehotencoding SET DiacritizedCharacterOneHotEncoding = REPLACE(DiacritizedCharacterOneHotEncoding, '\n', '');
+
+
+SET SQL_SAFE_UPDATES = 0;
+UPDATE undiaconehotencoding SET UnDiacritizedCharacterOneHotEncoding = REPLACE(UnDiacritizedCharacterOneHotEncoding, ' ', '');
+UPDATE undiaconehotencoding SET UnDiacritizedCharacterOneHotEncoding = REPLACE(UnDiacritizedCharacterOneHotEncoding, '[', '');
+UPDATE undiaconehotencoding SET UnDiacritizedCharacterOneHotEncoding = REPLACE(UnDiacritizedCharacterOneHotEncoding, ']', '');
+UPDATE undiaconehotencoding SET UnDiacritizedCharacterOneHotEncoding = REPLACE(UnDiacritizedCharacterOneHotEncoding, '\n', '');
+
+SET SQL_SAFE_UPDATES = 0;
+UPDATE distinctdiacritics SET encoding = REPLACE(encoding, ' ', '');
+UPDATE distinctdiacritics SET encoding = REPLACE(encoding, '[', '');
+UPDATE distinctdiacritics SET encoding = REPLACE(encoding, ']', '');
+
+
+delete from parseddocument where UnDiacritizedCharacter = 'bos' or UnDiacritizedCharacter = 'space' or UnDiacritizedCharacter = 'eos';
+delete from encodedwords where UnDiacritizedCharacter = 'bos' or UnDiacritizedCharacter = 'space' or UnDiacritizedCharacter = 'eos';
+delete from ListOfWordsAndSentencesInEachDoc where word = 'bos' or word = 'space' or word = 'eos';
+
+-- reset auto increment column
+SET SQL_SAFE_UPDATES = 0;
+SET  @num := 0;
+UPDATE distinctdiacritics SET id= @num := (@num+1);
+ALTER TABLE distinctdiacritics AUTO_INCREMENT =1;
+
+
+SET SQL_SAFE_UPDATES = 0;
+SET  @num := 0;
+UPDATE parseddocument SET idCharacterNumber= @num := (@num+1);
+ALTER TABLE parseddocument AUTO_INCREMENT =1;
+
+SET SQL_SAFE_UPDATES = 0;
+SET  @num := 0;
+UPDATE encodedwords SET id= @num := (@num+1);
+ALTER TABLE encodedwords AUTO_INCREMENT =1;
+
+SET SQL_SAFE_UPDATES = 0;
+SET  @num := 0;
+UPDATE ListOfWordsAndSentencesInEachDoc SET idWord= @num := (@num+1);
+ALTER TABLE ListOfWordsAndSentencesInEachDoc AUTO_INCREMENT =1;
+
+SET SQL_SAFE_UPDATES = 0;
+SET  @num := 0;
+UPDATE undiaconehotencoding SET idUnDiacritizedCharacter= @num := (@num+1);
+ALTER TABLE undiaconehotencoding AUTO_INCREMENT =1;
+
+SET SQL_SAFE_UPDATES = 0;
+SET  @num := 0;
+UPDATE diaconehotencoding SET idDiacritizedCharacter= @num := (@num+1);
+ALTER TABLE diaconehotencoding AUTO_INCREMENT =1;
+
+SET SQL_SAFE_UPDATES = 0;
+SET  @num := 0;
+UPDATE distinctdiacritics SET id= @num := (@num+1);
+ALTER TABLE distinctdiacritics AUTO_INCREMENT =1;
+
+SET SQL_SAFE_UPDATES = 0;
+SET  @num := 0;
+UPDATE dictionary SET idDictionary= @num := (@num+1);
+ALTER TABLE dictionary AUTO_INCREMENT =1;
+
+SET SQL_SAFE_UPDATES = 0;
+SET  @num := 0;
+UPDATE arabic_letters_with_diacritics SET id= @num := (@num+1);
+ALTER TABLE arabic_letters_with_diacritics AUTO_INCREMENT =1;
+
+-- the following "update" commands is for "atb3"
+-- update parseddocument set Diacritics='ٰ';file:/D:/Repos/MST/MST/Encoding/CreatingOneHotNew.py
+UPDATE parseddocument SET Diacritics = REPLACE(Diacritics,'ٰ','');
+UPDATE parseddocument SET Diacritics = REPLACE(Diacritics,'ًً','ً');
+UPDATE parseddocument SET DiacritizedCharacter = REPLACE(DiacritizedCharacter,'اًً','اً');
+
+-- Dictionary Table Creation
+insert into dictionary (DiacritizedWord,UnDiacritizedWord)
+(select word, UnDiacritizedWord from parseddocument where   LetterType='training' group by word order by UnDiacritizedWord asc);
+
+-- presence of shadda only mistake
+select  distinct word from parseddocument where lettertype='testing' and diacritics = 'ْ'
--- Queries.sql
+++ Queries.sql
@@ -1,94 +1,61 @@
+-- General Selection
+select * from parseddocument;
+select * from encodedwords;
+select * from undiaconehotencoding;
+select * from distinctdiacritics;
+select * from alldiacriticsinalldocuments;
+select * from arabic_letters_without_diacritics;
+select * from arabic_letters_with_diacritics;
+select * from arabic_diacritics;
+select * from labels;
+select * from dictionary;
+
+
+-- Conditional Selection
+
+-- Parsed Table
 select * from parseddocument where   LetterType='training' ;
 select * from parseddocument where   LetterType='validation';
 select  * from parseddocument where LetterType='testing';
-SELECT SentenceNumber, COUNT(idCharacterNumber) FROM parseddocument GROUP BY SentenceNumber ORDER BY SentenceNumber ASC;
-SELECT SentenceNumber, COUNT(*)
-FROM parseddocument
-GROUP BY SentenceNumber;
-
-select distinct SentenceNumber, Word from parseddocument;
-select * from parseddocument where SentenceNumber >29 and SentenceNumber < 35;
-select Word from parseddocument where DocName = 'ANN20021015.0101.txt' order by idCharacterNumber;
+select * from parseddocument where LetterType='testing';
 
 -- this represent alef when it is found above character, this is found only in atb3
 select  * from parseddocument where Diacritics= 'ٰ';
 select  * from parseddocument where UnDiacritizedCharacter= 'ـ';
 select  * from parseddocument where Diacritics= 'ًً';
 
--- the following "update" commands is for "atb3"
--- update parseddocument set Diacritics='ٰ';
-UPDATE parseddocument SET Diacritics = REPLACE(Diacritics,'ٰ','');
+-- shadda error in atb3
+select distinct diacritics from parseddocument where Diacritics = 'ّ';
 
-UPDATE parseddocument SET Diacritics = REPLACE(Diacritics,'ًً','ً');
-UPDATE parseddocument SET DiacritizedCharacter = REPLACE(DiacritizedCharacter,'اًً','اً');
+-- fatha correction words and letters in parsed table
+select distinct word from parseddocument where UnDiacritizedCharacter = (select arabic_letter from arabic_letters_without_diacritics where id = 7);
+select distinct word, UnDiacritizedCharacter from parseddocument where UnDiacritizedCharacter = (select arabic_letter from arabic_letters_without_diacritics where id = 2);
+select distinct word, UnDiacritizedCharacter from parseddocument where UnDiacritizedCharacter = (select arabic_letter from arabic_letters_without_diacritics where id = 34);
 
-select * from encodedwords;
 
-select * from undiaconehotencoding;
+-- UnDiacOneHotEncoding table
 select * from UnDiacOneHotEncoding where UnDiacritizedCharacter='' or UnDiacritizedCharacter='.';
+
+
+-- diaconehotencoding table
 select * from diaconehotencoding order by DiacritizedCharacter asc;
 
-select * from dictionary;
 
+-- ListOfWordsAndSentencesInEachDoc table
 select * from ListOfWordsAndSentencesInEachDoc where word = '' ;
-select * from ListOfWordsAndSentencesInEachDoc;
-select distinct SentenceNumber, DocName from listofwordsandsentencesineachdoc;
-
-select * from distinctdiacritics;
-
-select * from alldiacriticsinalldocuments;
+select * from ListOfWordsAndSentencesInEachDoc where word != 'space' and word != 'bos' and word != 'eos' ;
 
 
-select * from arabic_letters_without_diacritics;
-select * from arabic_letters_with_diacritics;
-select * from arabic_diacritics;
-select * from labels;
-
-
-SET NAMES 'utf8' COLLATE 'utf8_general_ci';
-ALTER DATABASE mstdb CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
-
--- reset auto increment column
-SET SQL_SAFE_UPDATES = 0;
-SET  @num := 0;
-UPDATE parseddocument SET idCharacterNumber= @num := (@num+1);
-ALTER TABLE parseddocument AUTO_INCREMENT =1;
-
-SET SQL_SAFE_UPDATES = 0;
-SET  @num := 0;
-UPDATE encodedwords SET id= @num := (@num+1);
-ALTER TABLE encodedwords AUTO_INCREMENT =1;
-
-SET SQL_SAFE_UPDATES = 0;
-SET  @num := 0;
-UPDATE ListOfWordsAndSentencesInEachDoc SET idWord= @num := (@num+1);
-ALTER TABLE ListOfWordsAndSentencesInEachDoc AUTO_INCREMENT =1;
-
-
-SET SQL_SAFE_UPDATES = 0;
-delete  from parseddocument;
-SET SQL_SAFE_UPDATES = 0;
-delete  from encodedwords;
-SET SQL_SAFE_UPDATES = 0;
-delete  from ListOfWordsAndSentencesInEachDoc;
-SET SQL_SAFE_UPDATES = 0;
-delete  from diaconehotencoding;
-SET SQL_SAFE_UPDATES = 0;
-delete  from undiaconehotencoding;
-SET SQL_SAFE_UPDATES = 0;
-delete from alldiacriticsinalldocuments;
-SET SQL_SAFE_UPDATES = 0;
-delete from distinctdiacritics;
-SET SQL_SAFE_UPDATES = 0;
-delete from dictionary;
-SET SQL_SAFE_UPDATES = 0;
-delete from arabic_letters_with_diacritics;
-SET SQL_SAFE_UPDATES = 0;
-delete from arabic_diacritics;
-SET SQL_SAFE_UPDATES = 0;
-delete from labels;
-
+-- dictionary table
+select DiacritizedWord from dictionary where  UnDiacritizedWord = 'إنا';
+select * from dictionary where DiacritizedWord ='فَتَمَنَّوُا';
+select * from dictionary where DiacritizedWord ='وَرَأَوُا';
+select * from dictionary where DiacritizedWord ='يَرَوُا';
+select * from dictionary where DiacritizedWord ='تَحَرَّوْا';
+select * from dictionary where DiacritizedWord ='اهْتَدَوْا';
+select * from dictionary where DiacritizedWord ='تَحَرَّوْا';
+select * from dictionary where DiacritizedWord ='كُفُوًا';
+select * from dictionary where DiacritizedWord ='هُزُوًا';
 
 
 CREATE TABLE new_foo LIKE parseddocument;
@@ -102,45 +69,3 @@ DROP TABLE old_foo;
 CREATE TABLE new_foo LIKE ListOfWordsAndSentencesInEachDoc;
 RENAME TABLE ListOfWordsAndSentencesInEachDoc TO old_foo, new_foo TO ListOfWordsAndSentencesInEachDoc;
 DROP TABLE old_foo;
-
-SET SQL_SAFE_UPDATES = 0;
-UPDATE diaconehotencoding SET DiacritizedCharacterOneHotEncoding = REPLACE(DiacritizedCharacterOneHotEncoding, ' ', '');
-UPDATE diaconehotencoding SET DiacritizedCharacterOneHotEncoding = REPLACE(DiacritizedCharacterOneHotEncoding, '[', '');
-UPDATE diaconehotencoding SET DiacritizedCharacterOneHotEncoding = REPLACE(DiacritizedCharacterOneHotEncoding, ']', '');
-UPDATE diaconehotencoding SET DiacritizedCharacterOneHotEncoding = REPLACE(DiacritizedCharacterOneHotEncoding, '\n', '');
-
-
-SET SQL_SAFE_UPDATES = 0;
-UPDATE undiaconehotencoding SET UnDiacritizedCharacterOneHotEncoding = REPLACE(UnDiacritizedCharacterOneHotEncoding, ' ', '');
-UPDATE undiaconehotencoding SET UnDiacritizedCharacterOneHotEncoding = REPLACE(UnDiacritizedCharacterOneHotEncoding, '[', '');
-UPDATE undiaconehotencoding SET UnDiacritizedCharacterOneHotEncoding = REPLACE(UnDiacritizedCharacterOneHotEncoding, ']', '');
-UPDATE undiaconehotencoding SET UnDiacritizedCharacterOneHotEncoding = REPLACE(UnDiacritizedCharacterOneHotEncoding, '\n', '');
-
-SET SQL_SAFE_UPDATES = 0;
-UPDATE distinctdiacritics SET encoding = REPLACE(encoding, ' ', '');
-UPDATE distinctdiacritics SET encoding = REPLACE(encoding, '[', '');
-UPDATE distinctdiacritics SET encoding = REPLACE(encoding, ']', '');
-
-
-
-ALTER TABLE ListOfWordsAndSentencesInEachDoc CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
-insert into diaconehotencoding (idDiacritizedCharacter, DiacritizedCharacter) values (422, 'عًّ');
-insert into alldiacriticsinalldocuments (Diacritics) values ('عًّ');
-UPDATE alldiacriticsinalldocuments SET Diacritics = SUBSTR(Diacritics, 1);
-insert into dictionary (DiacritizedWord,UnDiacritizedWord)(select word, UnDiacritizedWord from parseddocument group by word order by UnDiacritizedWord asc);
-
-
-select distinct SentenceNumber, DocName from listofwordsandsentencesineachdoc;
-
-update parseddocument Set LetterType = 'training' where idCharacterNumber < 1172931;
-
-update parseddocument Set LetterType = 'testing' where idCharacterNumber >= 1172931;
-
-select distinct DocName, SentenceNumber from parseddocument where LetterType = 'testing' order by  idCharacterNumber asc;
-
-select * from 
-listofwordsandsentencesineachdoc where DocName='ANN20021015.0100.txt'
-
-
-
