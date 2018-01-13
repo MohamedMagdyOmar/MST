b7a07ef98942a3e0049a7a546d90fe8f3838972e
MohamedMagdyOmar <eng_mohamed_magdy@hotmail.com>
2018-01-13 16:23:50 +0200

    add new one hot encoding

--- PostProcessingQueries.sql
+++ PostProcessingQueries.sql
@@ -17,12 +17,11 @@ UPDATE distinctdiacritics SET encoding = REPLACE(encoding, '[', '');
 UPDATE distinctdiacritics SET encoding = REPLACE(encoding, ']', '');
 
 SET SQL_SAFE_UPDATES = 0;
-delete from parseddocument where UnDiacritizedCharacter = 'bos' or UnDiacritizedCharacter = 'space' or UnDiacritizedCharacter = 'eos';
-delete from encodedwords where UnDiacritizedCharacter = 'bos' or UnDiacritizedCharacter = 'space' or UnDiacritizedCharacter = 'eos';
-delete from ListOfWordsAndSentencesInEachDoc where word = 'bos' or word = 'space' or word = 'eos';
-delete from arabic_letters_without_diacritics where arabic_letter = 'bos' or arabic_letter = 'space' or arabic_letter = 'eos';
-delete from distinctdiacritics where id > 14;
-delete from diaconehotencoding where idDiacritizedCharacter > 479;
+UPDATE diacritics_and_undiacritized_letter_one_hot_encoding SET OneHotEncoding = REPLACE(OneHotEncoding, ' ', '');
+UPDATE diacritics_and_undiacritized_letter_one_hot_encoding SET OneHotEncoding = REPLACE(OneHotEncoding, '[', '');
+UPDATE diacritics_and_undiacritized_letter_one_hot_encoding SET OneHotEncoding = REPLACE(OneHotEncoding, ']', '');
+
+
 -- reset auto increment column
 SET SQL_SAFE_UPDATES = 0;
 SET  @num := 0;
@@ -44,6 +43,12 @@ SET SQL_SAFE_UPDATES = 0;
 SET  @num := 0;
 UPDATE ListOfWordsAndSentencesInEachDoc SET idWord= @num := (@num+1);
 ALTER TABLE ListOfWordsAndSentencesInEachDoc AUTO_INCREMENT =1;
+
+SET SQL_SAFE_UPDATES = 0;
+SET  @num := 0;
+UPDATE diacritics_and_undiacritized_letter_one_hot_encoding SET id= @num := (@num+1);
+ALTER TABLE diacritics_and_undiacritized_letter_one_hot_encoding AUTO_INCREMENT =1;
+
 
 SET SQL_SAFE_UPDATES = 0;
 SET  @num := 0;
@@ -90,4 +95,16 @@ select  distinct idCharacterNumber, Diacritics from parseddocument where diacrit
 
 UPDATE parseddocument
 SET Diacritics = 'َّ' 
-WHERE Diacritics = 'ّ' 
+WHERE Diacritics = 'ّ' ;
+
+SET SQL_SAFE_UPDATES = 0;
+UPDATE parseddocument
+SET DiacritizedCharacter = 'َّ' 
+WHERE DiacritizedCharacter = 'ّ' ;
+
+SET SQL_SAFE_UPDATES = 0;
+UPDATE parseddocument
+SET DiacritizedCharacter = 'تَّ'
+WHERE DiacritizedCharacter = 'تّ' 
+
+
